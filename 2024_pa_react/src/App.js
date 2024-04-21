c3f1beafb7e57d87c109caa2bcf4e7055322cdaf
Zharks <nevraddacunha@myges.fr>
2024-04-21 16:13:51 +0200

    feat: button myspace + not authorised page v2

--- 2024_pa_react/src/App.js
+++ 2024_pa_react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
@@ -7,21 +7,43 @@ import DonationPage from "./pages/DonationPage.tsx";
 import MembershipPage from "./pages/MembershipPage.tsx";
 import UserInfoPage from "./pages/UserInfoPage.tsx";
 import UserDonation from "./pages/UserDonation.tsx";
+import NotAuthorizedPage from "./pages/NotAuthorizedPage.tsx";
+import Cookies from "js-cookie";
+
+const PrivateRoute = ({ children }) => {
+    const authToken = Cookies.get("authToken");
+    return authToken ? children : <Navigate to="/not-authorized" replace />;
+};
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/donation" element={<DonationPage />} />
-            <Route path="/membership" element={<MembershipPage />} />
-            <Route path="/user/info" element={<UserInfoPage />} />
-            <Route path="/user/donation" element={<UserDonation />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/donation" element={<DonationPage />} />
+                <Route path="/membership" element={<MembershipPage />} />
+                <Route path="/not-authorized" element={<NotAuthorizedPage />} />
+                <Route
+                    path="/user/info"
+                    element={
+                        <PrivateRoute>
+                            <UserInfoPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/user/donation"
+                    element={
+                        <PrivateRoute>
+                            <UserDonation />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
